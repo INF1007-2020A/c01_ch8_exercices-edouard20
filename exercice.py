9f7ada022a039885bcc852795837c701ff9477d7
EdouardFrechette <71038294+edouard20@users.noreply.github.com>
2020-10-26 14:20:26 -0400

    first func

--- exercice.py
+++ exercice.py
@@ -7,9 +7,19 @@
 
 
 # TODO: Définissez vos fonction ici
-
-
+def compare_files(first_file: str,sec_file: str):
+    with open(first_file,"r", encoding="utf-8") as f1, open(sec_file,"r",encoding="utf-8") as f2:
+        same = True
+        while same:
+            a = f1.read()
+            b = f2.read()
+            same = a==b
+        print(same)
+    return -1 if same else f1.tell()
+    #     for _ in first_file:
+    #         if f1.read(1) != f2.read(1):
+    #             return "false"
+    # return "true"
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-    pass
+    print(compare_files("text.txt","text1.txt"))
